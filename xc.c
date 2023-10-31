22aa22803f6ce61d63308256830dabd99079f5ff
phancb <nxhsdu@qq.com>
2023-10-31 15:53:30 +0800

    impl calculator

--- xc.c
+++ xc.c
@@ -50,6 +50,87 @@ enum { CHAR, INT, PTR };
 int *idmain; // the `main` function
 
 void next() {
+  // skip white space
+  while (*src == ' ' || *src == '\t') {
+    src++;
+  }
+
+  token = *src++;
+
+  if (token >= '0' && token <= '9') {
+    token_val = token - '0';
+    token = Num;
+
+    while (*src >= '0' && *src <= '9') {
+      token_val = token_val * 10 + *src - '0';
+      src++;
+    }
+    return;
+  }
+}
+
+void match(int tk) {
+  if (token != tk) {
+    printf("expected token: %d(%c), got: %d(%c)\n", tk, tk, token, token);
+    exit(-1);
+  }
+  next();
+}
+
+int expr();
+
+int factor() {
+  int value = 0;
+  if (token == '(') {
+    match('(');
+    value = expr();
+    match(')');
+  } else {
+    value = token_val;
+    match(Num);
+  }
+  return value;
+}
+
+int term_tail(int lvalue) {
+  if (token == '*') {
+    match('*');
+    int value = lvalue * factor();
+    return term_tail(value);
+  } else if (token == '/') {
+    match('/');
+    int value = lvalue / factor();
+    return term_tail(value);
+  } else {
+    return lvalue;
+  }
+}
+
+int term() {
+  int lvalue = factor();
+  return term_tail(lvalue);
+}
+
+int expr_tail(int lvalue) {
+  if (token == '+') {
+    match('+');
+    int value = lvalue + term();
+    return expr_tail(value);
+  } else if (token == '-') {
+    match('-');
+    int value = lvalue - term();
+    return expr_tail(value);
+  } else {
+    return lvalue;
+  }
+}
+
+int expr() {
+  int lvalue = term();
+  return expr_tail(lvalue);
+}
+
+void _next() {
   char *last_pos;
   int hash;
 
@@ -400,7 +481,7 @@ int loadfile(const char *filename) {
   return 0;
 }
 
-int main(int argc, char **argv) {
+int _main(int argc, char **argv) {
   argc--;
   argv++;
 
@@ -477,4 +558,14 @@ int main(int argc, char **argv) {
   pc = text;
 
   return eval();
+}
+
+int main(int argc, char *argv[]) {
+  size_t linecap = 0;
+  ssize_t linelen;
+  while ((linelen = getline(&src, &linecap, stdin)) > 0) {
+    next();
+    printf("%d\n", expr());
+  }
+  return 0;
 }
